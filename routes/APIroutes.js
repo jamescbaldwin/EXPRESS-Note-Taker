223815c3ab55c4194134ac94154769edc9b23305
James Baldwin <jamescbaldwin@gmail.com>
2020-11-28 19:59:19 -0500

    testing save function

--- routes/APIroutes.js
+++ routes/APIroutes.js
@@ -1,5 +1,5 @@
-const db = require("../db/db.json");
-const fs = require("fs");
+var db = require("../db/db.json");
+var fs = require("fs");
 
 //Routing
 module.exports = function (app) {
@@ -20,12 +20,12 @@ module.exports = function (app) {
     });
 
     app.delete("/api/notes/:id", function (req, res) {
-        let id = req.params.id;
+        var id = req.params.id;
         db.splice(id -1, 1);
         db.forEach((obj, i) => {
             obj.id = i + 1;
         });
-        fs.writeFile("./db/db.json", JSON.stringify(db), function () {
+        fs.writeFile("./db/db.json", JSON.stringify(db), function() {
             res.json(db);
         });
     });
