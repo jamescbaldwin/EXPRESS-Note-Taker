6dbcadc395e1b8b17b0c6a9226e96582234cb6c5
James Baldwin <jamescbaldwin@gmail.com>
2020-11-30 20:15:19 -0500

    db.json tinkering

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 var express = require("express");
 
-require('./routes/HTMLroutes')(app);
-require('./routes/APIroutes')(app);
-
 var app = express();
 
 var PORT = process.env.PORT || 3001;
@@ -10,6 +7,9 @@ var PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
+
+require('./routes/HTMLroutes')(app);
+require('./routes/APIroutes')(app);
 
 app.listen(PORT, function () {
     console.log('Application listening on PORT: ' + PORT);
